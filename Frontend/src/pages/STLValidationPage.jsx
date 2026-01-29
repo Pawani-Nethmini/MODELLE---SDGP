61cb0fe9730ff23cfd92580f2e5647648e1bef35
pamuduNethisa004 <pamudunj2022@gmail.com>
2026-01-29 11:57:40 +0530

    Update robot mouse interaction, Integrate background video and improve responsive design

--- Frontend/src/pages/STLValidationPage.jsx
+++ Frontend/src/pages/STLValidationPage.jsx
@@ -1,38 +1,21 @@
-import { useState } from "react";
 import STLUploadCard from "../components/STLUploadCard";
 import InteractiveRobot from "../components/InteractiveRobot";
-import STLValidatorPanel from "../components/STLValidatorPanel";
-import "../styles/stlValidator.css";
-
+import BackgroundShapes from "../components/BackgroundShapes";
+import "../styles/stlvalidation.css";
 
 export default function STLValidationPage() {
-  const [uploadedFile, setUploadedFile] = useState(null);
   return (
     <>
-
-      <section className="ValidatorScreen">
-        {/* Robot section */}
-          <section
-            style={{
-              height: "50vh",
-              width: "100%",
-              position: "relative",
-              overflow: "hidden",
-              pointerEvents: "none", 
-              marginTop: "-150px"
-            }}
-          >
-            <InteractiveRobot />
-          </section>
-
-            
-          {/* Upload section */}
-          {!uploadedFile && <STLUploadCard onFileUpload={setUploadedFile}/>}
-          
-          {/* validator panel with already uploaded file */}
-          {uploadedFile && <STLValidatorPanel file={uploadedFile} />}
+      {/* Full page 3D background - behind everything */}
+      <BackgroundShapes />
+      
+      {/* Robot section */}
+      <section className="robot-section">
+        <InteractiveRobot />
       </section>
-   
+
+      {/* Upload section */}
+      <STLUploadCard />
     </>
   );
 }
