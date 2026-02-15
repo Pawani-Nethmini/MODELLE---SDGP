3dc940eb2e183057fd77d5c2dd16cb38c55e8875
Pawani-Nethmini <pawaninethmini969@gmail.com>
2026-02-15 22:21:50 +0530

    Configure MongoDB connection and secure environment variables

--- backend/api/src/server.js
+++ backend/api/src/server.js
@@ -10,6 +10,7 @@ import dotenv from "dotenv";
 dotenv.config(); 
 
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -17,6 +18,8 @@ console.log("ENV CHECK:", {
   MERCHANT_ID: process.env.PAYHERE_MERCHANT_ID,
   SECRET: process.env.PAYHERE_MERCHANT_SECRET
 });
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
